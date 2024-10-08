6a8dde85d92e53e6f8467d700314586834537a5f
AgusVenturelli <agustin.venturelli@mercadolibre.com>
2024-10-07 22:03:41 -0300

    chore(nvim): add lsp config for golang

--- dotfiles/.config/nvim/lua/plugins/lsp.lua
+++ dotfiles/.config/nvim/lua/plugins/lsp.lua
@@ -3,6 +3,17 @@ return {
     "neovim/nvim-lspconfig",
     opts = {
       format = { timeout_ms = 5000 },
+      servers = {
+        gopls = {
+          settings = {
+            gopls = {
+              analyses = {
+                fieldalignment = false,
+              },
+            },
+          },
+        },
+      },
     },
   },
 }
